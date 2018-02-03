6d0067f7b79054e3642b5dd65787f7d04bb7a1f5
brambu <s@brambu.com>
2018-02-03 09:58:22 -0800

    updating packages

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements():
 
 setup(
     name='thedenbot',
-    version='0.0.8',
+    version='0.0.9',
     description='a telegram bot',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
